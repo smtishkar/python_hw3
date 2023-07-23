f1641ca6c1697b283fdee240e671e4226e36df76
unknown <smtishkar@mail.ru>
2023-07-23 22:54:22 +0300

    отформатировал код

--- task3.py
+++ task3.py
@@ -1,9 +1,10 @@
-# Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения. 
+# Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения.
 # Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность. Достаточно
 #  вернуть один допустимый вариант. *Верните все возможные варианты комплектации рюкзака.
 from itertools import permutations
 
-my_dict = {'котелок': 3, 'палатка': 7, 'плитка': 4, 'посуда': 3, 'удочка': 4, 'книги': 3, 'топор': 5}
+my_dict = {'котелок': 3, 'палатка': 7, 'плитка': 4,
+           'посуда': 3, 'удочка': 4, 'книги': 3, 'топор': 5}
 MAX_CAPACITY = 15
 weight = 0
 
@@ -14,20 +15,20 @@ weight = 0
 #     weight += my_dict[things]
 #     if weight <= MAX_CAPACITY:
 #         print(f"{things} - {values} кг")
-          
 
-#Второй вариант. Поиск всех возможных вариантов
+
+# Второй вариант. Поиск всех возможных вариантов
 my_things_list = []
 new_list = []
-common_list=[]
+common_list = []
 final_list = []
 
 for keys in my_dict.keys():
     my_things_list.append(keys)
 
-weight = 0 
-for things in permutations(my_things_list,len(my_things_list)):
-    weight = 0 
+weight = 0
+for things in permutations(my_things_list, len(my_things_list)):
+    weight = 0
     for i in things:
         a = my_dict.get(i)
         weight += my_dict.get(i)
--- task4.py
+++ task4.py
@@ -6,19 +6,19 @@
 # Код должен расширяться на любое большее количество друзей.
 
 
-dict_frends_atribute = {"Алексей": ("мяч", "посуда", "котелок",),\
-                        "Витя": ("вода", "мяч", "котелок",),\
-                        "Сергей": ("плитка", "котелок", "мангал",),\
-                         "Коля" :  ("ружье", "уголь", "мясо",)   }
+dict_frends_atribute = {"Алексей": ("мяч", "посуда", "котелок",),
+                        "Витя": ("вода", "мяч", "котелок",),
+                        "Сергей": ("плитка", "котелок", "мангал",),
+                        "Коля":  ("ружье", "уголь", "мясо",)}
 
 friend_list = []
 all_things = []
 unique_things = []
-temp_except=[]
+temp_except = []
 absent_things = []
 
 for keys in dict_frends_atribute:
-   friend_list.append(keys)
+    friend_list.append(keys)
 
 # Формируем список всех вещей
 for keys, values in dict_frends_atribute.items():
@@ -32,7 +32,7 @@ for keys, values in dict_frends_atribute.items():
         if i != keys:
             temp_except = set(temp_except).union(set(dict_frends_atribute[i]))
     unique_things = set(dict_frends_atribute[keys]).difference(temp_except)
-    print(f" Уникальная вещь для {keys} - {unique_things}")
+    print(f"Уникальная вещь для {keys} - {unique_things}")
     temp_except.clear()
     unique_things.clear()
 
@@ -43,6 +43,6 @@ for keys, values in dict_frends_atribute.items():
         if i != keys:
             temp_except = set(temp_except).union(set(dict_frends_atribute[i]))
     absent_things = temp_except.difference(set(dict_frends_atribute[keys]))
-    print(f" У {keys} нет следующих вещей - {absent_things}")
+    print(f"У {keys} нет следующих вещей - {absent_things}")
     temp_except.clear()
     absent_things.clear()
